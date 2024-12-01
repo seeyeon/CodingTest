c9731d887616067fed233631eb22dc82c209950a
seeyeon <71398979+seeyeon@users.noreply.github.com>
2024-12-01 20:05:45 +0900

    Title: 함수를 이용한 윤년 판별, Time: 107ms, Memory: 10MB, Status: Passed - CodeTree

--- 241201/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 241201/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -12,11 +12,12 @@ public class Main {
     }
 
     public static boolean isYear(int y){
-        if(y%4==0)
-            return true;
         
         if(y%100==0 && y%400!=0)
             return false;
+
+        if(y%4==0)
+            return true;
 
         return false;
     }
