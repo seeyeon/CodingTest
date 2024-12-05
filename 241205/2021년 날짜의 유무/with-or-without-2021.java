7a84a9b9b1b4b57a694aa3f375605a66c8ed5cad
seeyeon <71398979+seeyeon@users.noreply.github.com>
2024-12-05 12:48:16 +0900

    Title: 2021년 날짜의 유무, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 241205/2021년 날짜의 유무/with-or-without-2021.java
+++ 241205/2021년 날짜의 유무/with-or-without-2021.java
@@ -22,6 +22,7 @@ public class Main {
     public static int getMaxDay(int month){
         if(month == 2) return 28;
         if(month == 4 || month == 6 || month == 9 || month == 11) return 30;
+        if(month >12) return 0;
         return 31;
     }
 
