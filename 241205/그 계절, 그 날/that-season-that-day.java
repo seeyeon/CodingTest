f27cd0714c4d64e79eef2102c75c4a223366b170
seeyeon <71398979+seeyeon@users.noreply.github.com>
2024-12-05 17:27:04 +0900

    Title: 그 계절, 그 날, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241205/그 계절, 그 날/that-season-that-day.java
+++ 241205/그 계절, 그 날/that-season-that-day.java
@@ -39,9 +39,9 @@ public class Main {
         if(month==4 || month==6 || month==9 || month==11 ) return 30;
         
         if(month ==2){
+            if(year%400 ==0 ) return 29;
+            if(year%100 ==0) return 28;
             if(year%4 ==0 ) return 29;
-            if(year%4 ==0 && year%100 ==0) return 28;
-            if((year%4 ==0 && year%100 ==0) && year%400 ==0) return 29;
             else return 28;
         }
 
