30ba65a6a1689c6ed77517cae01e483cfb6029de
seeyeon <71398979+seeyeon@users.noreply.github.com>
2024-12-06 12:48:01 +0900

    Title: 2개 이상의 알파벳, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241206/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+
+        cal(A);
+    }
+
+    public static void cal(String a){
+        int cnt=0;
+
+        for(int i=0; i<a.length()-1; i++){
+            if(a.charAt(i) !=a.charAt(i+1)){
+                cnt++;
+            }
+        }
+
+        if(cnt>=2){
+            System.out.print("Yes");
+        }else{
+            System.out.print("NO");
+        }
+
+
+
+
+    }
+}
