6b8214cbd77ab0f6e412df8710f1fac27a7c66d6
seeyeon <71398979+seeyeon@users.noreply.github.com>
2024-12-10 00:07:49 +0900

    Title: 특정 구간의 합, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241210/특정 구간의 합/sum-of-specific-intervals.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static final int MAX_N = 100;
+
+    public static int[] arr = new int[MAX_N];
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<m; i++){
+            int a1 = sc.nextInt();
+            int a2 = sc.nextInt();
+
+            rangeSum(arr,a1,a2);
+        }
+    }
+
+    public static void rangeSum(int[] arr,int a, int b){
+        int sum=0;
+
+         for(int i=a-1; i<=b-1; i++){
+            sum +=arr[i];    
+         }
+
+         System.out.println(sum);
+    }
+}
