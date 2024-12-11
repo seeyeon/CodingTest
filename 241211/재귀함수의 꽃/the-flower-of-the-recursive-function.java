11ee31dbc7d8b72866a77787777ac27e43681e9c
seeyeon <71398979+seeyeon@users.noreply.github.com>
2024-12-11 14:57:14 +0900

    Title: 재귀함수의 꽃, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241211/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        numberPrint(n);
+    }
+
+    public static void numberPrint(int n){
+
+        if(n==0)
+            return;
+
+
+        System.out.print(n+" ");
+        numberPrint(n-1);
+        System.out.print(n+" ");
+    }
+}
