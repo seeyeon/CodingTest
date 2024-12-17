2182c3aaecdb30e77a955080bdbc2e8e515d12ee
seeyeon <71398979+seeyeon@users.noreply.github.com>
2024-12-17 14:52:56 +0900

    Title: 키를 기준으로 정렬, Time: 277ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241217/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+class Person{
+    String name, height, weight;
+
+    Person(String name, String height, String weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Person[] person = new Person[n];
+
+        for(int i=0; i<n; i++){
+            String name = sc.next();
+            String height = sc.next();
+            String weight = sc.next();
+
+            person[i] = new Person(name,height,weight);
+        }
+
+        Arrays.sort(person, Comparator.comparing(p->p.height));
+
+        for(int i=0; i<n; i++){
+            System.out.println(person[i].name + " "+person[i].height+" "+person[i].weight);
+        }
+    }
+}
