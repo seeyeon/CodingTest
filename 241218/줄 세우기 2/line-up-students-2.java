47dd15db79f764ddec8a54e128918b457888bd49
seeyeon <71398979+seeyeon@users.noreply.github.com>
2024-12-18 12:26:28 +0900

    Title: 줄 세우기 2, Time: 160ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241218/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Student implements Comparable<Student>{
+    int h,w,num;
+
+    public Student(int h,int w, int num){
+        this.h=h;
+        this.w=w;
+        this.num=num;
+    }
+
+    @Override
+    public int compareTo(Student student){
+        if( this.h != student.h)
+            return this.h - student.h;
+
+        return student.w - this.w;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Student[] student = new Student[n];
+
+        for(int i=0; i<n; i++){
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int num = i+1;
+
+            student[i] = new Student(h,w,num);
+        }
+
+        Arrays.sort(student);
+
+        for(int i=0; i<n; i++){
+            System.out.println(student[i].h+" "+student[i].w+" "+student[i].num);
+        }
+    }
+}
