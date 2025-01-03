8bfbb947112d2235ef4acc80deea87815fb1a383
seeyeon <71398979+seeyeon@users.noreply.github.com>
2025-01-03 14:07:01 +0900

    Title: 문자열 추가하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250103/문자열 추가하기/add-spring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        String a = "Hello";
+
+        System.out.print(str+a);
+
+    }
+}
