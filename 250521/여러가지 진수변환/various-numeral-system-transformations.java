2cd54d8a75709e002fe3b6d1be310d9ef895cbfc
seeyeon <71398979+seeyeon@users.noreply.github.com>
2025-05-21 17:19:06 +0900

    Title: 여러가지 진수변환, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

--- 250521/여러가지 진수변환/various-numeral-system-transformations.java
+++ 250521/여러가지 진수변환/various-numeral-system-transformations.java
@@ -6,5 +6,21 @@ public class Main {
         int n = sc.nextInt();
         int b = sc.nextInt();
         // Please write your code here.
+        int[] digit = new int[20];
+        int cnt =0;
+
+        while(true){
+            if(n<b){
+                digit[cnt++] = n;
+                break;
+            }
+
+            digit[cnt++] = n%b;
+            n /=b;
+        }
+
+        for(int i=cnt-1; i>=0; i--){
+            System.out.print(digit[i]);
+        }
     }
 }
