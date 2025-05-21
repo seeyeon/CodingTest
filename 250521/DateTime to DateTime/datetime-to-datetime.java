9742af681461217d3dbadea1ce011c09e470e801
seeyeon <71398979+seeyeon@users.noreply.github.com>
2025-05-21 11:12:06 +0900

    Title: DateTime to DateTime, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250521/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        // Please write your code here.
+
+        if(A<11 || B<11 || C<11){
+            System.out.print("-1");
+        }
+
+        int day = (A-11)*24*60;
+        int hour = (B-11)*60;
+        int min = (C-11);
+
+        int result = day+hour+min;
+
+        System.out.print(result);
+    }
+}
